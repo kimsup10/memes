a2dae1d17ce6761abcbcd6a8fb557d3c84886e87
hhk <hhk2945@naver.com>
2016-11-20 08:21:54 -0800

    유저모델 추가 + login UI 구현

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ module.exports = function (app) {
     app.use('/user', users);
     app.use('/uploads', imgUpload);
 
+
     app.get('/', function(req, res, next) {
         Attachment.findAll({limit: 10}).then(function(attachments) {
             res.render('index', { attachments: attachments });
         });
+
+
     });
 }
