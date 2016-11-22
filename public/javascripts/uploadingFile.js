359cc483f72f8edc3e2f3c6e51cff0af0cd8af61
Kimsup10 <kss5662@ajou.ac.kr>
2016-11-22 14:42:37 +0900

    modified removeAttachment

--- public/javascripts/uploadingFile.js
+++ public/javascripts/uploadingFile.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var imgTarget = $('.input-group .uploadImg');
     var docsTarget = $('.input-group .uploadDocs');
-    var uploadCount = 0;
+    var uploadId=0;
     imgTarget.on('change', function() {
         var files = $(this)[0].files;
         if (!$(this)[0].files[0].type.match(/image\//))
@@ -13,24 +13,39 @@ $(document).ready(function(){
                 showPreview(files[i]);
             }
         }
+        $('button')[0].disabled=false;
     });
 
     function showPreview(file){
         var reader = new FileReader();
-        reader.onload = myOnload;
+        var thumbnailId = file.name.replace(/(\.)|(\s)/g, '-');
+
+        reader.onload = function myOnload(e){
+            var src = e.target.result;
+            $('.uploadImgList').append('<div class="uploads-display" id='+thumbnailId+'><div class="uploads-thumb-wrap"><img src="'+src+'" class="uploads-thumb"></div></div>');
+        };
+
         reader.addEventListener('load', function () {
-            $('.uploads-display#'+(uploadCount++)).append('<input type="textarea" name ="description" class="form-control" placeholder="'+file.name+'에 대해 해쉬태그와 함께 설명해주세요!">' +
+            $('.uploads-display#'+thumbnailId).append('<input type="textarea" name ="description" class="form-control" placeholder="'+file.name+'에 대해 해쉬태그와 함께 설명해주세요!">' +
                 '<p class="btn-primary" name="cancel" onclick="removeAttachment(this.parentNode.id)">취소하기</p><br>');
         });
         reader.readAsDataURL(file);
     }
 
-    function myOnload(e){
-        var src = e.target.result;
-        $('.uploadImgList').append('<div class="uploads-display" id="'+(uploadCount)+'"><div class="uploads-thumb-wrap"><img src="'+src+'" class="uploads-thumb"></div></div>');
-    }
+
 });
 
 function removeAttachment(id) {
-    document.getElementsByClassName('uploads-display')[id].innerHTML='';
+    var thumbnail = document.getElementById(id);
+    var files = document.getElementById('attachFile').files[0]='';
+
+    // TODO: Delete files from input FileList.
+    for (var i in files){
+        if (files[i].name != id)
+            document.getElementById('attachFile').files[i]='';
+    }
+
+    document.getElementsByClassName('uploadImgList')[0].removeChild(thumbnail);
+    if ($('.uploads-display').length==0)
+        document.getElementsByTagName('button')[0].disabled=true;
 }
