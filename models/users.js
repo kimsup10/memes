fb15d455f0580ae8faed3a53f08d41dbbb79b353
Hunchul Park <huntrax11@gmail.com>
2016-11-22 14:23:29 +0900

    Implemented signup

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
 var db = require('../utils/database.js');
 
 var User = db.define('user', {
@@ -8,13 +9,29 @@ var User = db.define('user', {
     autoIncrement: true
   },
   username: Sequelize.STRING,
-  password: Sequelize.STRING,
-  email: Sequelize.STRING
+  password: {
+    type: Sequelize.STRING,
+    set: function(val) {
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(val, salt);
+      this.setDataValue('password', hash);
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {isEmail: true}
+  }
 }, {
   tableName: 'users',
   underscored: true,
   instanceMethods: {
-  }
+    authenticate: function(value) {
+      if (bcrypt.compareSync(value, this.password))
+        return this;
+      else
+        return false;
+    }
+  },
 });
 
 module.exports = User;
