2fe5ba188c20445225597d71e99e1b8743e7ce35
mac <mac@macui-MacBook-Pro.local>
2016-11-25 21:52:08 +0900

    Implemented Image serving by node.js

--- utils/multer.js
+++ utils/multer.js
@@ -6,7 +6,7 @@ var storage = multer.diskStorage({
         cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname + '-' + uuid())
+        cb(null, uuid() )
     }
 });
 
