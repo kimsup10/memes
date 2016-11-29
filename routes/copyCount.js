8405b59b2f97dcd481d4ab233b07f383bc26b84c
Kimsup10 <kss5662@ajou.ac.kr>
2016-11-30 01:50:18 +0900

    implemented trend ranking based on reddit hot algorithm

--- routes/copyCount.js
+++ routes/copyCount.js
@@ -7,14 +7,38 @@ var redisClient = redis.createClient();
 var router = express.Router();
 
 router.post('/', function (req, res) {
-    redisClient.zincrby("copy_count", 1, req.body.attachmentId, function (err, reply) {
+    var key = 'attach#'+req.body.attachmentId;
+    redisClient.hincrby(key, 'copy_count', 1, function (err, reply) {
         if (err) res.send(500);
         else{
+            redisClient.hgetall(key, function (err, obj) {
+                if (err) res.send(500);
+                else{
+                    trendingRank(key, obj);
+                }
+            });
             console.log(reply.toString());
             res.send(200);
         }
     });
 });
 
+function trendingRank(member, hash) {
+    // Set seconds_score
+    if (hash.seconds_score){
+        var seconds_score = hash.seconds_score;
+    } else{
+        var seconds_score = Math.round(new Date().getTime()/(1000*45000));
+        redisClient.hset(key, 'seconds_score', seconds_score)
+    }
+    var count_score = Math.log10(hash.copy_count);
+
+    var score = Number(count_score) + Number(seconds_score);
+
+    console.log(count_score, seconds_score, score);
+    redisClient.zadd('trending', score, member);
+}
+
 
 module.exports = router;
+
